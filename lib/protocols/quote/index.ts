7a7649fe3d49f9752ccc9d4f996413ea0f594eb8
nymmrx <nymmrx@protonmail.com>
2021-03-25 15:37:30 +0100

    feat: new oracle

--- lib/protocols/quote/index.ts
+++ lib/protocols/quote/index.ts
@@ -5,7 +5,7 @@ import BigNumber from "bignumber.js";
 
 import aliases from "./aliases.json";
 
-export const QuoteAddress = "0x89ECCe31817c2B98479Ba36694810c4497ADA361";
+export const QuoteAddress = "0x9b8b9F6146B29CC32208f42b995E70F0Eb2807F3";
 
 export const USDC = {
   address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
